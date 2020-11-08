57d3dadea9f8c0dbfeca06ed034946efadde4004
kottypalacios <34010688+kottypalacios@users.noreply.github.com>
2020-11-07 23:59:48 -0300

    Modificacion de Header  y Home

--- src/app/componets/header/header.component.ts
+++ src/app/componets/header/header.component.ts
@@ -1,11 +1,37 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, HostBinding, HostListener } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+
   @Input() deviceXs: boolean;
+
+
+  isFixedNavbar;
+  @HostBinding('class.navbar-opened') navbarOpened = false;
+  constructor(
+  ) { }
+  ngOnInit() {
+  }
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const offset = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+    if(offset > 10) {
+      this.isFixedNavbar = true;
+    } else {
+      this.isFixedNavbar = false;
+    }
+  }
+
+  toggleNavbar() {
+    this.navbarOpened = !this.navbarOpened;
+  }
+
 
 }
